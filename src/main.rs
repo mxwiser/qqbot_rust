2fd1a71fe6f8c366c59e8d701b940187d991244d
mxwiser <neoway@126.com>
2025-03-14 07:40:50 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 mod bot_web_hook;
 
-use std::time::Duration;
 
-use crate::bot_web_hook::bot_error;
-use crate::bot_web_hook::info;
-use bot_web_hook::message::MessageHelper;
-use bot_web_hook::{BotHook, message::MessageEvent};
+
+use bot_web_hook::{BotHook, message::MessageEvent,info,bot_error,message,bot_trait};
 
 
 
-use bot_web_hook::bot_trait::BotTrait;
-impl BotTrait for BotHook {
+impl bot_trait::BotTrait for BotHook {
     async  fn message_process(_message_event: &MessageEvent) -> Result<(), bot_error::Error> {
         let _t =_message_event.t.as_ref().unwrap();
         if _t.as_ref() == "GROUP_AT_MESSAGE_CREATE".to_string()
@@ -20,7 +16,9 @@ impl BotTrait for BotHook {
             let _id = _d.author.as_ref().unwrap().id.as_ref().unwrap();
             let _text = _d.content.as_ref().unwrap();
             info!("收到消息 ID:",_id," 内容: ",_text);
-            MessageHelper::rot_message(&_text, _message_event).await?;
+
+            message::MessageHelper::rot_message(&_text, _message_event).await.unwrap();
+
         }
         Ok(())
     }
@@ -29,6 +27,5 @@ impl BotTrait for BotHook {
 
 #[tokio::main]
 async fn main() {
-
      BotHook::start().await;
 }
