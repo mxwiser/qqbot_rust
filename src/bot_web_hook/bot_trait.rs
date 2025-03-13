b9a119794aad2d96cde4493c355c8f8a615ddd7c
mxwiser <neoway@126.com>
2025-03-13 20:42:43 +0800

    update

--- src/bot_web_hook/bot_trait.rs
+++ src/bot_web_hook/bot_trait.rs
@@ -1,8 +1,5 @@
-use super::BotHook;
 use super::MessageEvent;
 use super::bot_error;
-use super::message::MessageHelper;
-use super::info;
 
 pub trait BotTrait{
   async fn message_process(_message_event: &MessageEvent) -> Result<(), bot_error::Error>;
